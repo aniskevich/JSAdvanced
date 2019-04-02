f661de79b9e443db670cdda75e20905784655fa3
Boris Aniskevich <aniskevich.bo@gmail.com>
2019-04-03 00:55:07 +1000

    HW5

--- public/script.js
+++ public/script.js
@@ -1,109 +1,46 @@
-class Item {
-    constructor(name, price, link) {
-        this.name = name;
-        this.price = price;
-        this.link = link;
-    }
-    addToCart() {
-
-    }
-    render() {
-        return `<div class="item"><img src="${this.link}"><div class="itemText"><h4>${this.name}</h4>
-        <h3>$${this.price}.00</h3></div><div class="fade"><button>Add to Cart</button></div></div>`;
-    }
-}
-
-class ItemsList {
-    constructor() {
-        this.items = [];
-    }
-    getItems() {
-        return new Promise((resolve, reject) => {
-            sendRequest('http://localhost:3000/catalogData.json').then((response) => {
-                return this.items = JSON.parse(response).map(item => new Item(item.name, item.price, item.link));
-            }).then(() => resolve(this.items));
-        });        
-    }
-    render() {
-        const itemsHtml = this.items.map(item => item.render());
-        return document.querySelector('.itemsLayout').innerHTML = itemsHtml.join('');   
-    }
-}
-
-class ItemInCart {
-    constructor(name, price, link, color, size, quantity, subtotal) {
-        this.link = link;
-        this.name = name;
-        this.color = color;
-        this.size = size;
-        this.price = price;
-        this.quantity = quantity;
-        this.subtotal = subtotal;
-    }
-    render() {
-        this.getSubTotal(this.price, this.quantity);
-        return `<div><span>${this.name}</span><span>${this.price}</span><span>${this.link}</span>
-        <span>${this.color}</span><span>${this.size}</span><span>${this.quantity}</span><span>${this.subtotal}</span></div>`;
-    }
-    getSubTotal(price, quantity) {
-        return this.subtotal = price * quantity;
-    }
-    removeOne(quantity) {
-        //доделать удаление единицы товара
-    }
-}
-
-class CartList {
-    constructor() {
-        this.items = [];
-    }
-    render() {
-        const itemsHtml = this.items.map(item => item.render());
-        return document.querySelector('.shopCart').innerHTML = itemsHtml.join('');
-    }
-    getItems() {
-        return new Promise((resolve, reject) => {
-            sendRequest('http://localhost:3000/getBasket.json').then((response) => {
-                return this.items = JSON.parse(response).map(item => new ItemInCart(item.name, item.price, item.link, item.color, item.size, item.quantity, item.subtotal));
-            }).then(() => resolve(this.items));
-        }); 
-    }
-    getTotal() {
-        return this.items.reduce((acc, item) => acc + item.subtotal, 0);
-    }
-    clearCart() {
-        this.items = [];
-    }
-}
-
-function sendRequest(url) {
-    return new Promise((resolve, reject) => {
-        const xhr = new XMLHttpRequest();
-        xhr.open('GET', url);
-        xhr.send();
-        xhr.onreadystatechange = () => {
-            if(xhr.readyState === XMLHttpRequest.DONE) {
-                if(xhr.status === 200) {
-                    resolve(xhr.responseText);
-                }
-                else {
-                    reject(alert(xhr.status));
+const app = new Vue({
+    el: '#app',
+    data: {
+        mainNav: ['home', 'men', 'women', 'kids', 'accessoriese', 'featured', 'hot deals'],
+        catalogData: [],
+        cart: [],
+        search: '',
+    },
+    methods: {
+        addToCart: function(event) {
+            if(this.cart.includes(this.catalogData[event.target.id - 1])) {
+                for(item in this.cart) {
+                    if(this.cart[item].id == (event.target.id)) {
+                        this.cart[item].quantity++;
+                        this.cart[item].subtotal = this.cart[item].price * this.cart[item].quantity;
+                    }
                 }
             }
+            else {
+                this.cart.push(this.catalogData[event.target.id - 1]);
+                for(item in this.cart) {
+                   if(this.cart[item].id == (event.target.id)) {
+                        this.$set(this.cart[item], 'quantity', 1);
+                        this.cart[item].subtotal = this.cart[item].price;
+                    }
+                }
+            }
+        },
+        getTotal: function() {
+            return this.cart.reduce((acc, product) => acc + product.subtotal, 0);
+        },
+    },
+    computed: {
+        filteredCatalogData() {
+            const regexp = new RegExp(this.search, 'i');
+            return this.catalogData.filter((product) => regexp.test(product.name));
         }
-    }); 
-}
-
-function init() {
-    const items = new ItemsList();
-    items.getItems().then(()=>{items.render();});
-    
-    const $proceedToCartBtn = document.createElement('button');
-    document.querySelector('.proceedToCartBtn').appendChild($proceedToCartBtn);
-    $proceedToCartBtn.innerHTML = 'Proceed to Cart';
-
-    const cartItems = new CartList();
-    cartItems.getItems().then(() => {cartItems.render();});
-}
-
-window.onload = function() {init()};
+    },
+    mounted() {
+        fetch('http://localhost:3000/catalogData')
+            .then((response) => response.json())
+            .then((catalogData) => {
+                this.catalogData = catalogData;
+        });
+    }
+}); 
